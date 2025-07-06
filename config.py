9146be66f56e8cf54169ded3740cc277020f2758
bairagi2oo3 <tusharbairage95@gmail.com>
2025-07-06 21:44:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/bairagi2oo3/ShizukaMusicBot4",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
